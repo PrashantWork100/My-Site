261d201b41ddb45283f977860bcd1acc0ebe85e5
Prashant Sutar <prashant.work100@gmail.com>
2024-01-16 19:27:46 +0530

    Dark Theme

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+// function jumpToSection(sectionId) {
+//     const sectionElement = document.getElementById(sectionId);
+
+//     if (sectionElement) {
+//       sectionElement.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   }
+
 function jumpToSection(sectionId) {
     const sectionElement = document.getElementById(sectionId);
 
     if (sectionElement) {
-      sectionElement.scrollIntoView({ behavior: 'smooth' });
+      const marginTop = 20; // Adjust the margin top as needed
+      const newPosition = sectionElement.offsetTop - marginTop;
+
+      window.scrollTo({
+        top: newPosition,
+        behavior: 'smooth'
+      });
     }
   }
